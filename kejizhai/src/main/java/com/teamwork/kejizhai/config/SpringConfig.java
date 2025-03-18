a1f7eb66057049298bfbdd89d759872882ebb02a
dyh0001 <dyh276179395@outlook.com>
2025-03-18 22:44:03 +0800

    ？

--- kejizhai/src/main/java/com/teamwork/kejizhai/config/SpringConfig.java
+++ kejizhai/src/main/java/com/teamwork/kejizhai/config/SpringConfig.java
@@ -1,2 +1,36 @@
-package com.teamwork.kejizhai.config;public class SpringConfig {
+package com.teamwork.kejizhai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+        
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+        
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js/");
+        
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
 }
--- kejizhai/src/main/java/com/teamwork/kejizhai/config/WebConfig.java
+++ kejizhai/src/main/java/com/teamwork/kejizhai/config/WebConfig.java
@@ -1,17 +1,14 @@
 package com.teamwork.kejizhai.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
-        registry.addViewController("/index").setViewName("forward:/index.html");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
-    
-    // Web相关配置
 }
